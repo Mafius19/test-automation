1c931874553915013ffd5b711ed986811fa0a376
Matias Guerrero <matias.f.guerrero19@gmail.com>
2021-08-26 15:55:41 -0300

    Ej 2 realizado

--- src/test/java/Ejercicios.java
+++ src/test/java/Ejercicios.java
@@ -6,10 +6,20 @@ public class Ejercicios {
     /*Ejercicio 1:
     Crear un método que abra el browser y muestre el explorador de google con Chrome*/
     @Test
-    public void Ejercicio1() {
+    public void ejercicio1() {
        System.setProperty("webdriver.chrome.driver","drivers/chromedriver.exe");
        WebDriver driver = new ChromeDriver();
        driver.get("https://www.facebook.com");
     }
 
+    /*Ejercicio 2
+    Crear un método llamado mostrarTituloTest, que muestre el título del sitio web.*/
+    @Test
+    public void mostrarTituloTest() {
+        System.setProperty("webdriver.chrome.driver","drivers/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.google.com");
+        System.out.println("Título de la pagina: " + driver.getTitle());
+        System.out.println("Título de la pagina: " + driver.getCurrentUrl());
+    }
 }
