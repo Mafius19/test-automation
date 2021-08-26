cea5a1c3c3e5c4c67a45f919842b541dca61090e
Matias Guerrero <matias.f.guerrero19@gmail.com>
2021-08-26 16:22:43 -0300

    Ej 3 finalizado

--- src/test/java/Ejercicios.java
+++ src/test/java/Ejercicios.java
@@ -1,9 +1,13 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
+import java.util.List;
+
 public class Ejercicios {
-    /*Ejercicio 1:
+    /*Ejercicio 1
     Crear un método que abra el browser y muestre el explorador de google con Chrome*/
     @Test
     public void ejercicio1() {
@@ -21,5 +25,39 @@ public class Ejercicios {
         driver.get("https://www.google.com");
         System.out.println("Título de la pagina: " + driver.getTitle());
         System.out.println("Título de la pagina: " + driver.getCurrentUrl());
+    }
+
+    /*Ejercicio 3
+    Crear un método llamado bbcMundTest
+    Ingresar al sitio https://www.bbc.com/mundo
+    Mostrar la cantidad de <h1>
+    Imprimir títulos h1
+    Mostrar la cantidad de <h2>
+    Imprimir títulos h2*/
+    @Test
+    public void bbcMundTest() {
+        String URL_BBC = "https://www.bbc.com/mundo";
+        System.setProperty("webdriver.chrome.driver","drivers/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get(URL_BBC);
+
+        List<WebElement> listaH1s = driver.findElements(By.tagName("h1"));
+        System.out.println("La cantidad de elementos H1 del sitio son: " + listaH1s.size());
+        System.out.println("****** Elementos H1 ******");
+        for (WebElement H1:listaH1s) {
+            System.out.println(H1.getText());
+        }
+        System.out.println("**************************");
+
+
+        List<WebElement> listaH2s = driver.findElements(By.tagName("h2"));
+        System.out.println("La cantidad de elementos H2 del sitio son: " + listaH2s.size());
+        System.out.println("****** Elementos H2 ******");
+        for (WebElement H1:listaH2s) {
+            System.out.println(H1.getText());
+        }
+        System.out.println("**************************");
+
+        driver.close();
     }
 }
