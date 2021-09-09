30c4a8384ae02dcb238000d87c9aa3d89a9d7f15
Matias Guerrero <matias.f.guerrero19@gmail.com>
2021-09-09 16:45:55 -0300

    Ejercicio 2 realizado

--- src/test/java/Practico_9.java
+++ src/test/java/Practico_9.java
@@ -32,4 +32,20 @@ public class Practico_9 {
         Assert.assertNotEquals(driver.getCurrentUrl(), "https://www.facebook.com/", "Se deberia estar en otra url" );
 
     }
+
+    /*Ejercicio 2
+    Crear un método de test llamado forgotAccountPartialLinkTest
+    Ir a Facebook https://www.facebook.com/
+    Hacer click en “¿Olvidaste” utilizando el partialLink
+    Agregar un assert que el título sea “¿Olvidaste tu contraseña? | No puedo iniciar sesión | Facebook”*/
+    @Test
+    public void forgotAccountPartialLinkTest() {
+        WebDriver driver = getDriver("https://www.facebook.com");
+        driver.findElement(By.partialLinkText("¿Olvidaste")).click();
+
+        Assert.assertEquals(driver.getTitle(),
+                "¿Olvidaste tu contraseña? | No puedo iniciar sesión | Facebook",
+                "Se debería estar en la sección de olvidaste tu contraseña");
+    }
+
 }
